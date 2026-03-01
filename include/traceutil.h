27b525a8ea6c1309aae02daf51025475c165d0ca
Dieter J Kybelksties <github@kybelksties.com>
2026-03-01 14:55:15 +0000

    modernised tracing

--- include/traceutil.h
+++ include/traceutil.h
@@ -9,100 +9,100 @@
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- *
- * @date: 2023-08-28
- * @author: Dieter J Kybelksties
  */
 
-//
-// NOTE: Include guards removed so that individual files can get traced, even if the file was previously included
-//
+#ifndef DKYB_TRACEUTIL_H_
+#define DKYB_TRACEUTIL_H_
 
 #include <iostream>
+#include <string_view>
+#include <utility>
+#if __has_include(<source_location>)
+#include <source_location>
+#if defined(__cpp_lib_source_location) && __cpp_lib_source_location >= 201907L
+#define DKYB_HAS_SOURCE_LOCATION 1
+#endif
+#endif
 
-// un-define all macros to silence preprocessor warnings
-#undef TRACE_STR11
+#ifndef DKYB_HAS_SOURCE_LOCATION
+#if __has_include(<experimental/source_location>)
+#include <experimental/source_location>
+#define DKYB_HAS_SOURCE_LOCATION 1
+#define DKYB_SOURCE_LOCATION_EXPERIMENTAL 1
+#endif
+#endif
+
+#include <unistd.h>
+#include <cstdint>
+
+namespace dkyb::trace {
+
+#ifdef DKYB_HAS_SOURCE_LOCATION
+#ifdef DKYB_SOURCE_LOCATION_EXPERIMENTAL
+    using source_location = std::experimental::source_location;
+#else
+    using source_location = std::source_location;
+#endif
+#else
+    struct source_location {
+        static constexpr source_location current(const char* file = __builtin_FILE(),
+                                                 std::uint32_t line = __builtin_LINE(),
+                                                 const char* func = __builtin_FUNCTION()) noexcept {
+            return {file, line, func};
+        }
+        constexpr source_location() noexcept : file("unknown"), ln(0), func("unknown") {}
+        constexpr source_location(const char* file, std::uint32_t line, const char* func) noexcept
+            : file(file), ln(line), func(func) {}
+        constexpr const char* file_name() const noexcept { return file; }
+        constexpr std::uint32_t line() const noexcept { return ln; }
+        constexpr const char* function_name() const noexcept { return func; }
+
+    private:
+        const char* file;
+        std::uint32_t ln;
+        const char* func;
+    };
+#endif
+
+    template<typename... Args>
+    inline void trace_out(source_location loc, bool show_pid, bool enabled, Args&&... args) {
+        if (enabled) {
+            std::cout << loc.file_name() << ":" << loc.line() << " " << loc.function_name();
+            if (show_pid) {
+                std::cout << " PID=" << getpid();
+            }
+            ((std::cout << " " << std::forward<Args>(args)), ...);
+            std::cout << std::endl;
+        }
+    }
+} // namespace dkyb::trace
+
+#endif // DKYB_TRACEUTIL_H_
+
+// The following part is NOT guarded to allow redefining TRACE macros when DO_TRACE_ changes
+#undef DKYB_TRACE_ENABLED_
 #undef TRACE0
 #undef TRACE1
 #undef TRACE2
 #undef TRACE3
-// multi process versions for forked processes
-#undef PTRACE_STR1
 #undef PTRACE0
 #undef PTRACE1
 #undef PTRACE2
 #undef PTRACE3
 
-#if defined DO_TRACE_
-    #define TRACE_STR1(v1)                                                                                             \
-        {                                                                                                              \
-            std::cout << __FILE__ << ":" << __LINE__ << " " << __FUNCTION__ << " " << #v1 << std::endl;                \
-        }
-    #define TRACE0                                                                                                     \
-        {                                                                                                              \
-            std::cout << __FILE__ << ":" << __LINE__ << " " << __FUNCTION__ << std::endl;                              \
-        }
-    #define TRACE1(v1)                                                                                                 \
-        {                                                                                                              \
-            std::cout << __FILE__ << ":" << __LINE__ << " " << __FUNCTION__ << " " << #v1 << "=" << v1 << std::endl;   \
-        }
-    #define TRACE2(v1, v2)                                                                                             \
-        {                                                                                                              \
-            std::cout << __FILE__ << ":" << __LINE__ << " " << __FUNCTION__ << " " << #v1 << "=" << v1 << " " << #v2   \
-                      << "=" << v2 << std::endl;                                                                       \
-        }
-    #define TRACE3(v1, v2, v3)                                                                                         \
-        {                                                                                                              \
-            std::cout << __FILE__ << ":" << __LINE__ << " " << __FUNCTION__ << " " << #v1 << "=" << v1 << " " << #v2   \
-                      << "=" << v2 << " " << #v3 << "=" << v3 << std::endl;                                            \
-        }
-    // multi process versions for forked processes
-    #define PTRACE_STR1(v1)                                                                                            \
-        {                                                                                                              \
-            std::cout << __FILE__ << ":" << __LINE__ << " " << __FUNCTION__ << " PID=" << getpid() << " " << #v1       \
-                      << std::endl;                                                                                    \
-        }
-    #define PTRACE0                                                                                                    \
-        {                                                                                                              \
-            std::cout << __FILE__ << ":" << __LINE__ << " " << __FUNCTION__ << " PID=" << getpid() << std::endl;       \
-        }
-    #define PTRACE1(v1)                                                                                                \
-        {                                                                                                              \
-            std::cout << __FILE__ << ":" << __LINE__ << " " << __FUNCTION__ << " PID=" << getpid() << " " << #v1       \
-                      << "=" << v1 << std::endl;                                                                       \
-        }
-    #define PTRACE2(v1, v2)                                                                                            \
-        {                                                                                                              \
-            std::cout << __FILE__ << ":" << __LINE__ << " " << __FUNCTION__ << " PID=" << getpid() << " " << #v1       \
-                      << "=" << v1 << " " << #v2 << "=" << v2 << std::endl;                                            \
-        }
-    #define PTRACE3(v1, v2, v3)                                                                                        \
-        {                                                                                                              \
-            std::cout << __FILE__ << ":" << __LINE__ << " " << __FUNCTION__ << " PID=" << getpid() << " " << #v1       \
-                      << "=" << v1 << " " << #v2 << "=" << v2 << " " << #v3 << "=" << v3 << std::endl;                 \
-        }
-
+#if defined(ENABLE_DEBUG_TRACE) || defined(DO_TRACE_)
+    #define DKYB_TRACE_ENABLED_ true
 #else
+    #define DKYB_TRACE_ENABLED_ false
+#endif
 
-    #define TRACE_STR1(v1)
-    #define TRACE0
-    #define TRACE1(v1)
-    #define TRACE2(v1, v2)
-    #define TRACE3(v1, v2, v3)
-    // multi process versions for forked processes
-    #define PTRACE_STR1(v1)
-    #define PTRACE0
-    #define PTRACE1(v1)
-    #define PTRACE2(v1, v2)
-    #define PTRACE3(v1, v2, v3)
+// Simple Wrappers
+#define TRACE0 dkyb::trace::trace_out(dkyb::trace::source_location::current(), false, DKYB_TRACE_ENABLED_)
+#define TRACE1(v1) dkyb::trace::trace_out(dkyb::trace::source_location::current(), false, DKYB_TRACE_ENABLED_, #v1 "=", v1)
+#define TRACE2(v1, v2) dkyb::trace::trace_out(dkyb::trace::source_location::current(), false, DKYB_TRACE_ENABLED_, #v1 "=", v1, #v2 "=", v2)
+#define TRACE3(v1, v2, v3) dkyb::trace::trace_out(dkyb::trace::source_location::current(), false, DKYB_TRACE_ENABLED_, #v1 "=", v1, #v2 "=", v2, #v3 "=", v3)
 
-#endif // DO_TRACE_
+#define PTRACE0 dkyb::trace::trace_out(dkyb::trace::source_location::current(), true, DKYB_TRACE_ENABLED_)
+#define PTRACE1(v1) dkyb::trace::trace_out(dkyb::trace::source_location::current(), true, DKYB_TRACE_ENABLED_, #v1 "=", v1)
+#define PTRACE2(v1, v2) dkyb::trace::trace_out(dkyb::trace::source_location::current(), true, DKYB_TRACE_ENABLED_, #v1 "=", v1, #v2 "=", v2)
+#define PTRACE3(v1, v2, v3) dkyb::trace::trace_out(dkyb::trace::source_location::current(), true, DKYB_TRACE_ENABLED_, #v1 "=", v1, #v2 "=", v2, #v3 "=", v3)
--- test/test_trace.cpp
+++ test/test_trace.cpp
@@ -1,9 +1,41 @@
+#include <iostream>
+
+// Scenario 1: Trace enabled
 #define DO_TRACE_
 #include <traceutil.h>
 
-int main() {
+void test_enabled() {
+    std::cout << "--- Scenario: Trace Enabled ---" << std::endl;
     int x = 42;
     TRACE1(x);
     TRACE0;
+}
+
+// Scenario 2: Trace disabled
+#undef DO_TRACE_
+#include <traceutil.h>
+
+void test_disabled() {
+    std::cout << "--- Scenario: Trace Disabled ---" << std::endl;
+    int y = 100;
+    TRACE1(y); // Should produce no output
+    TRACE0;    // Should produce no output
+}
+
+// Scenario 3: Trace re-enabled
+#define DO_TRACE_
+#include <traceutil.h>
+
+void test_reenabled() {
+    std::cout << "--- Scenario: Trace Re-enabled ---" << std::endl;
+    int z = 7;
+    TRACE1(z);
+    TRACE0;
+}
+
+int main() {
+    test_enabled();
+    test_disabled();
+    test_reenabled();
     return 0;
 }
